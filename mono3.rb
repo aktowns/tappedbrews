d7ce601c27953b5e808f9b5c5d83c04ecbf08d4a
Ashley Towns <ashleyis@me.com>
2012-10-31 21:44:45 +1100

    added caveats, fsharp3.0.11

--- mono3.rb
+++ mono3.rb
@@ -1,5 +1,10 @@
 require 'formula'
 
+class Fsharp < Formula
+  homepage 'http://fsharp.github.com/fsharp/'
+  url 'https://nodeload.github.com/fsharp/fsharp/zipball/3.0.11'
+end
+
 class Mono3 < Formula
   url 'http://download.mono-project.com/sources/mono/mono-3.0.0.tar.bz2'
   homepage 'http://www.mono-project.com/Release_Notes_Mono_3.0'
@@ -15,5 +20,14 @@ class Mono3 < Formula
     system "./configure", *args
     system "make"
     system "make install"
+
+    Fsharp.new.brew do
+      system "./autogen.sh --prefix=#{prefix}"
+      system "make install"
+    end
+  end
+
+  def caveats 
+    "Compiling may fail on static pthread error, try using --use-llvm"
   end
 end
